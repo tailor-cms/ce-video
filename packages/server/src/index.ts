84efaa032ca69b1e506367b64c8e5beb2664f0b2
rkusan00 <renato.kusanovic@gmail.com>
2025-07-09 13:05:22 +0200

    Migrate Server package

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -7,39 +7,36 @@ const IS_CEK = process.env.CEK_RUNTIME;
 // Don't use in production, use only when IS_CEK=true
 const USER_STATE: any = {};
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-export function beforeSave(element: Element, services: HookServices) {
+export function beforeSave(element: Element, _services: HookServices) {
   return element;
 }
 
-export function afterSave(element: Element, services: HookServices) {
+export function afterSave(element: Element, _services: HookServices) {
   return element;
 }
 
 export function afterLoaded(
   element: Element,
-  services: HookServices,
-  runtime: ServerRuntime,
+  _services: HookServices,
+  _runtime: ServerRuntime,
 ) {
   return element;
 }
 
 export function afterRetrieve(
   element: Element,
-  services: HookServices,
-  runtime: ServerRuntime,
+  _services: HookServices,
+  _runtime: ServerRuntime,
 ) {
   return element;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function beforeDisplay(element: Element, context: any) {
+export function beforeDisplay(_element: Element, context: any) {
   return { ...context, ...USER_STATE };
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function onUserInteraction(
-  element: Element,
+  _element: Element,
   context: any,
   payload: any,
 ): any {
