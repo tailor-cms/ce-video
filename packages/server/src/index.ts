2142f3d0a01b1678c23d74417c57e2dd6a81f157
rkusan00 <renato.kusanovic@gmail.com>
2026-08-12 15:03:53 +0200

    Migrate to the latest CEK version which introduced general feedback

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,8 +4,8 @@ import type {
   OnUserInteractionHook,
   ServerModule,
 } from '@tailor-cms/cek-common';
-import { initState, type } from '@tailor-cms/ce-video-manifest';
 import type { Element } from '@tailor-cms/ce-video-manifest';
+import manifest from '@tailor-cms/ce-video-manifest';
 
 // Detect if hooks are running in CEK (used for mocking end-system runtime)
 const IS_CEK = process.env.CEK_RUNTIME;
@@ -40,13 +40,10 @@ export const hookMap: HookMap<Element> = new Map(
 );
 
 const serverModule: ServerModule<Element> = {
-  type,
-  initState,
+  ...manifest,
   hookMap,
   beforeDisplay,
   onUserInteraction,
 };
 
 export default serverModule;
-
-export { type, initState };
